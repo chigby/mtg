b1f7de9401fbd6e872e8b7c08ead4abbf009552c
procyon <devnull@localhost>
2010-11-22 22:56:56 -0500

    Removed .py extension from executable.

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,81 +1,88 @@
+import sys
+
 from dingus import DingusTestCase
+#from bin.mtg import main
+#import bin.mtg as mod
 
-from bin.mtg import main
-import bin.mtg as mod
+from types import ModuleType
+mtg = ModuleType('mtg')
+exec open('bin/mtg') in mtg.__dict__
+sys.modules['mtg'] = mtg
+mod = mtg
 
-class DescribeOptionParsing(DingusTestCase(main)):
+class DescribeOptionParsing(DingusTestCase(mtg.main)):
     
     def setup(self):
         super(DescribeOptionParsing, self).setup()
 
     def should_print_help_if_no_args(self):
-        main(['./mtg.py'])
+        mtg.main(['./mtg.py'])
         assert mod.OptionParser().calls('print_help')
 
 
-class DescribeMainProgram(DingusTestCase(main, exclude=['OptionParser', 
-                                                        'card_flags'])):
+class DescribeMainProgram(DingusTestCase(mtg.main, exclude=['OptionParser', 
+                                                            'card_flags'])):
     
     def setup(self):
         super(DescribeMainProgram, self).setup()
 
     def should_make_card_request(self):
-        main(['./mtg.py', 'sengir', 'vampire'])
+        mtg.main(['./mtg.py', 'sengir', 'vampire'])
         assert mod.SearchRequest.calls('()', {'name': 'sengir,vampire'})
 
     def should_make_card_request_with_text(self):
-        main(['./mtg.py', '--text=trample'])
+        mtg.main(['./mtg.py', '--text=trample'])
         assert mod.SearchRequest.calls('()', {'text': 'trample'})
 
     def should_make_card_request_with_block(self):
-        main(['./mtg.py', '--block=zendikar'])
+        mtg.main(['./mtg.py', '--block=zendikar'])
         assert mod.SearchRequest.calls('()', {'block': 'zendikar'})
 
     def should_make_card_request_with_special(self):
-        main(['./mtg.py', '--type=scheme', '--special'])
+        mtg.main(['./mtg.py', '--type=scheme', '--special'])
         assert mod.SearchRequest.calls('()', {'type': 'scheme'}, special=True,
                                        exclude_other_colors=None)
 
     def should_make_card_request_with_exclude(self):
-        main(['./mtg.py', '--color=u', '-x'])
+        mtg.main(['./mtg.py', '--color=u', '-x'])
         print mod.SearchRequest.calls()
         assert mod.SearchRequest.calls('()', {'color': 'u'}, special=None,
                                        exclude_other_colors=True)
 
     def should_not_request_reminder(self):
-        main(['./mtg.py', '--text=trample', '--reminder'])
+        mtg.main(['./mtg.py', '--text=trample', '--reminder'])
         assert mod.SearchRequest.calls('()', {'text': 'trample'})
 
     def should_not_request_hidesets(self):
-        main(['./mtg.py', '--text=trample', '--hidesets'])
+        mtg.main(['./mtg.py', '--text=trample', '--hidesets'])
         assert mod.SearchRequest.calls('()', {'text': 'trample'})
 
     def should_not_pass_rulings_to_gatherer_request(self):
-        main(['./mtg.py', '--text=trample', '--rulings'])
+        mtg.main(['./mtg.py', '--text=trample', '--rulings'])
         assert mod.SearchRequest.calls('()', {'text': 'trample'})
 
     def should_show_cards(self):
-        main(['./mtg.py', 'sengir', 'vampire'])
+        mtg.main(['./mtg.py', 'sengir', 'vampire'])
         for card in mod.CardExtractor().extract():
             print card.calls()
             assert card.calls('show')
         
     def should_show_cards_and_rulings(self):
-        main(['./mtg.py', 'sengir', 'vampire', '--rulings'])
+        mtg.main(['./mtg.py', 'sengir', 'vampire', '--rulings'])
         assert mod.CardExtractor().calls('extract')[0].kwargs == \
             {'get_card_urls': True}
         for card in mod.CardExtractor().extract():
             assert card.calls('show')[0].kwargs == {'rulings': True}
 
     def should_show_cards_and_flavor(self):
-        main(['./mtg.py', 'natural', 'selection', '--flavor'])
+        mtg.main(['./mtg.py', 'natural', 'selection', '--flavor'])
         assert mod.CardExtractor().calls('extract')[0].kwargs == \
             {'get_card_urls': True}
         for card in mod.CardExtractor().extract():
             assert card.calls('show')[0].kwargs['flavor'] == True
 
     def should_show_cards_and_rulings(self):
-        main(['./mtg.py', 'tarmogoyf', '--reminder'])
+        mtg.main(['./mtg.py', 'tarmogoyf', '--reminder'])
         assert mod.CardExtractor().calls('extract')[0].kwargs == \
             {'get_card_urls': None}
         for card in mod.CardExtractor().extract():
