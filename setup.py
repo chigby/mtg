178f4e7036d68f5da0be1bdbf0489fb8dee1373a
procyon <devnull@localhost>
2011-01-10 22:24:58 -0500

    Added PyPi classifiers, renamed script in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name = 'mtg',
     packages = ['mtglib'],
     package_dir = {'mtglib': 'mtglib'},
-    scripts = ['bin/mtg.py'],
+    scripts = ['bin/mtg'],
     requires = ['httplib2', 'beautifulsoup'],
     version = mtglib.__version__,
     description = 'Console-based access to the Gatherer Magic Card Database.',
@@ -16,6 +16,22 @@ setup(
     author_email = 'cameron.higbynaquin@gmail.com',
     url = 'http://bitbucket.org/procyon/mtg/',
     keywords = ['mtg', 'magic', 'gatherer'],
-    classifiers = [],
-    long_description = ''
+    classifiers = ['Development Status :: 4 - Beta', 
+                   'Environment :: Console', 
+                   'Intended Audience :: End Users/Desktop',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Natural Language :: English', 'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.6', 'Topic :: Utilities',
+                   'Topic :: Internet :: WWW/HTTP', 
+                   'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+                   'Topic :: Games/Entertainment'],
+    long_description = '''\
+Console-based access to the Gatherer Magic Card Database
+--------------------------------------------------------
+
+Search for Magic cards from the command line.  Limit your results by
+card name, color, type, rules text, converted mana cost, power,
+toughness, or expansion set.  Rulings and flavor text also available.
+Clean interface and output.
+'''
 )
