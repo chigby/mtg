6a38f106a4d71b496efcca7e2cc60625c6ad2b04
procyon <devnull@localhost>
2011-11-25 16:46:30 -0500

    Refactor hybrid symbol extractor.

--- mtglib/card_extractor.py
+++ mtglib/card_extractor.py
@@ -24,8 +24,8 @@ class CardExtractor(object):
 
     def _text_to_symbol(self, text):
         if ' or ' in text:
-            return '({0})'.format('/'.join(l[:1] for l in text.replace(' or ', '/')
-                            .split('/')).lower())
+            return '({0})'.format('/'.join(
+                    l[:1] for l in text.split(' or ')).lower())
         return text[:1]
 
     def _textbox_manasymbol(self, text):
