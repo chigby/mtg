90cbe055e9f0722de7619fbd777a10e2b95d63b3
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2013-09-07 22:20:27 -0400

    Use helper function for testing equality.

--- tests/constants_test.py
+++ tests/constants_test.py
@@ -1,5 +1,7 @@
 import unittest
 
+from nose.tools import eq_
+
 from mtglib.constants import base_url, card_flags
 
 class DescribeConstants(unittest.TestCase):
@@ -7,8 +9,8 @@ class DescribeConstants(unittest.TestCase):
     def should_have_base_url(self):
         url = ('http://gatherer.wizards.com/Pages/Search/Default.aspx'
                '?output=standard&action=advanced&')
-        assert base_url == url
+        eq_(base_url, url)
 
     def should_have_card_flags(self):
-        assert card_flags == ['text', 'color', 'subtype', 'type', 'set', 'cmc',
-                              'power', 'tough', 'rarity', 'name', 'block']
+        eq_(card_flags, ['text', 'color', 'subtype', 'type', 'set', 'cmc',
+                         'power', 'tough', 'rarity', 'name', 'block'])
